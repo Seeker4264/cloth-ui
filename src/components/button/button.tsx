fc2b80a58d95f93c3b4d971c36253712f2383bd6
Seeker4264 <sebashmorales@gmail.com>
2025-04-10 11:19:17 -0400

    button component testing

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,1 +1,20 @@
-export const Button = () => {};
+import { MouseEventHandler } from "react";
+import "./../../styles.css";
+
+export interface ButtonProps {
+  text: string;
+  disabled: boolean;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+export const Button = ({ disabled, text, onClick }: ButtonProps) => {
+  return (
+    <button
+      className="px-3 py-2 bg-white border-2 border-purple-700 text-purple-700 rounded-lg cursor-pointer hover:bg-purple-50 active:bg-purple-200 duration-150"
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
