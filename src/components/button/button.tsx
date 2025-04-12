66ce01961faab462606603e80ebc7cdecc6e14d6
Seeker4264 <sebashmorales@gmail.com>
2025-04-12 01:08:49 -0400

    button component refactor

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,188 +1,80 @@
 import { useRef, MouseEventHandler, MouseEvent } from "react";
 import "./../../styles.css";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  text: string;
-  disabled: boolean;
-  onClick: MouseEventHandler<HTMLButtonElement>;
-}
-
-export interface ButtonMainProps {
+export interface ButtonProps {
   variant: "primary" | "secondary" | "secondaryAlt";
   text: string;
   disabled: boolean;
   onClick: MouseEventHandler<HTMLButtonElement>;
 }
 
-const ButtonPrimary: React.FC<ButtonProps> = ({
-  text,
-  disabled,
-  onClick,
-  ...props
-}) => {
-  const buttonRef = useRef<HTMLButtonElement>(null);
-
-  const createRipple = (e: MouseEvent<HTMLButtonElement>) => {
-    if (onClick) {
-      onClick(e);
-    }
-
-    const button = buttonRef.current;
-    if (!button) return;
-
-    const circle = document.createElement("span");
-    const diameter = Math.max(button.clientWidth, button.clientHeight);
-    const radius = diameter / 2;
-
-    circle.style.width = circle.style.height = `${diameter}px`;
-    circle.style.backgroundColor = `#CADAFA`;
-    circle.style.left = `${e.clientX - button.getBoundingClientRect().left - radius}px`;
-    circle.style.top = `${e.clientY - button.getBoundingClientRect().top - radius}px`;
-    circle.classList.add("ripple");
-
-    const ripple = button.getElementsByClassName("ripple")[0];
-    if (ripple) {
-      ripple.remove();
-    }
-
-    button.appendChild(circle);
-  };
-
-  return (
-    <button
-      className="relative overflow-hidden px-5 py-2 text-lg font-medium rounded-lg cursor-pointer bg-white border-2 border-[#2C6DE0] text-[#2C6DE0]
-      hover:border-[#498BFF] hover:text-[#498BFF] hover:bg-[#E7EEFF]
-      active:border-[#144DB1] active:text-[#144DB1] active:bg-[#D5E4FF]
-      disabled:border-[#707070] disabled:bg-[#F0F0F0] disabled:text-[#707070]
-      duration-150"
-      ref={buttonRef}
-      disabled={disabled}
-      onClick={createRipple}
-      {...props}
-    >
-      <span className="relative z-20">{text}</span>
-    </button>
-  );
-};
-
-const ButtonSecondary: React.FC<ButtonProps> = ({
-  text,
-  disabled,
-  onClick,
-  ...props
-}) => {
-  const buttonRef = useRef<HTMLButtonElement>(null);
-
-  const createRipple = (e: MouseEvent<HTMLButtonElement>) => {
-    if (onClick) {
-      onClick(e);
-    }
-
-    const button = buttonRef.current;
-    if (!button) return;
-
-    const circle = document.createElement("span");
-    const diameter = Math.max(button.clientWidth, button.clientHeight);
-    const radius = diameter / 2;
-
-    circle.style.width = circle.style.height = `${diameter}px`;
-    circle.style.backgroundColor = `#1F60CF`;
-    circle.style.left = `${e.clientX - button.getBoundingClientRect().left - radius}px`;
-    circle.style.top = `${e.clientY - button.getBoundingClientRect().top - radius}px`;
-    circle.classList.add("ripple");
-
-    const ripple = button.getElementsByClassName("ripple")[0];
-    if (ripple) {
-      ripple.remove();
-    }
-
-    button.appendChild(circle);
-  };
-
-  return (
-    <button
-      className="relative overflow-hidden px-[1.375rem] py-2.5 text-lg font-medium rounded-lg cursor-pointer bg-[#447FE4] text-white
-      hover:bg-[#498BFF]
-      active:bg-[#2567DA]
-      disabled:bg-[#707070] disabled:text-[#CCCCCC]
-      duration-150"
-      ref={buttonRef}
-      disabled={disabled}
-      onClick={createRipple}
-      {...props}
-    >
-      <span className="relative z-20">{text}</span>
-    </button>
-  );
-};
-
-const ButtonSecondaryAlt: React.FC<ButtonProps> = ({
-  text,
-  disabled,
-  onClick,
-  ...props
-}) => {
-  const buttonRef = useRef<HTMLButtonElement>(null);
-
-  const createRipple = (e: MouseEvent<HTMLButtonElement>) => {
-    if (onClick) {
-      onClick(e);
-    }
-
-    const button = buttonRef.current;
-    if (!button) return;
-
-    const circle = document.createElement("span");
-    const diameter = Math.max(button.clientWidth, button.clientHeight);
-    const radius = diameter / 2;
-
-    circle.style.width = circle.style.height = `${diameter}px`;
-    circle.style.backgroundColor = `#7DA0E0`;
-    circle.style.left = `${e.clientX - button.getBoundingClientRect().left - radius}px`;
-    circle.style.top = `${e.clientY - button.getBoundingClientRect().top - radius}px`;
-    circle.classList.add("ripple");
-
-    const ripple = button.getElementsByClassName("ripple")[0];
-    if (ripple) {
-      ripple.remove();
-    }
-
-    button.appendChild(circle);
-  };
-
-  return (
-    <button
-      className="relative overflow-hidden px-[1.375rem] py-2.5 text-lg font-medium rounded-full cursor-pointer bg-[#AECBFF] text-[#003EAA]
-      hover:bg-[#9EBFFB]
-      active:bg-[#94B6F5]
-      disabled:bg-[#707070] disabled:text-[#CCCCCC]
-      duration-150"
-      ref={buttonRef}
-      disabled={disabled}
-      onClick={createRipple}
-      {...props}
-    >
-      <span className="relative z-20">{text}</span>
-    </button>
-  );
-};
-
-export const Button = ({
+export const Button: React.FC<ButtonProps> = ({
   variant = "primary",
   text,
   disabled,
   onClick,
-}: ButtonMainProps) => {
-  if (variant == "primary")
-    return <ButtonPrimary text={text} disabled={disabled} onClick={onClick} />;
+  ...props
+}) => {
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
-  if (variant == "secondary")
-    return (
-      <ButtonSecondary text={text} disabled={disabled} onClick={onClick} />
-    );
+  const buttonClass = {
+    primary: `relative overflow-hidden px-5 py-2 text-lg font-medium rounded-lg cursor-pointer bg-white border-2 border-[#2C6DE0] text-[#2C6DE0]
+      hover:border-[#498BFF] hover:text-[#498BFF] hover:bg-[#E7EEFF]
+      active:border-[#144DB1] active:text-[#144DB1] active:bg-[#D5E4FF]
+      disabled:border-[#707070] disabled:bg-[#F0F0F0] disabled:text-[#707070]
+      duration-150`,
+    secondary: `relative overflow-hidden px-[1.375rem] py-2.5 text-lg font-medium rounded-lg cursor-pointer bg-[#447FE4] text-white
+      hover:bg-[#498BFF]
+      active:bg-[#2567DA]
+      disabled:bg-[#707070] disabled:text-[#CCCCCC]
+      duration-150`,
+    secondaryAlt: `relative overflow-hidden px-[1.375rem] py-2.5 text-lg font-medium rounded-full cursor-pointer bg-[#AECBFF] text-[#003EAA]
+      hover:bg-[#9EBFFB]
+      active:bg-[#94B6F5]
+      disabled:bg-[#707070] disabled:text-[#CCCCCC]
+      duration-150`,
+  };
 
-  if (variant == "secondaryAlt")
-    return (
-      <ButtonSecondaryAlt text={text} disabled={disabled} onClick={onClick} />
-    );
+  const createRipple = (e: MouseEvent<HTMLButtonElement>) => {
+    if (onClick) {
+      onClick(e);
+    }
+
+    const button = buttonRef.current;
+    if (!button) return;
+
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+
+    circle.style.width = circle.style.height = `${diameter}px`;
+    const rippleColor = {
+      primary: "CADAFA",
+      secondary: "1F60CF",
+      secondaryAlt: "7DA0E0",
+    };
+    circle.style.backgroundColor = rippleColor[variant];
+    circle.style.left = `${e.clientX - button.getBoundingClientRect().left - radius}px`;
+    circle.style.top = `${e.clientY - button.getBoundingClientRect().top - radius}px`;
+    circle.classList.add("ripple");
+
+    const ripple = button.getElementsByClassName("ripple")[0];
+    if (ripple) {
+      ripple.remove();
+    }
+
+    button.appendChild(circle);
+  };
+
+  return (
+    <button
+      className={buttonClass[variant]}
+      ref={buttonRef}
+      disabled={disabled}
+      onClick={createRipple}
+      {...props}
+    >
+      <span className="relative z-20">{text}</span>
+    </button>
+  );
 };
