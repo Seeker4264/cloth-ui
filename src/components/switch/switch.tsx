f33b8a94540b1db83aa3e8102833af3db537073b
Seeker4264 <sebashmorales@gmail.com>
2025-04-13 12:24:41 -0400

    change switch component from button to checkbox tag

--- src/components/switch/switch.tsx
+++ src/components/switch/switch.tsx
@@ -1,30 +1,37 @@
 export interface SwitchProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  id: string;
   disabled?: boolean;
   checked: boolean;
-  onClick: () => void;
+  onChange: () => void;
 }
 
 export const Switch: React.FC<SwitchProps> = ({
+  id,
   disabled = false,
   checked,
-  onClick,
+  onChange,
   ...props
 }) => {
   return (
-    <button
-      className="group p-0.5 cursor-pointer"
-      disabled={disabled}
-      onClick={onClick}
-      {...props}
-    >
-      <div
-        className={`flex w-12 h-fit p-1 rounded-full ${checked ? "bg-[#459CFF] group-hover:bg-[#6FB2FF]" : "bg-[#888888] group-hover:bg-[#AAAAAA]"} duration-150`}
+    <>
+      <input
+        id={id}
+        type="checkbox"
+        className="group w-0 h-0 hidden"
+        disabled={disabled}
+        checked={checked}
+        onChange={onChange}
+        {...props}
+      />
+      <label
+        htmlFor={id}
+        className={`flex items-center cursor-pointer w-12 h-fit p-1 rounded-full ${checked ? "bg-[#459CFF] group-hover:bg-[#6FB2FF]" : "bg-[#888888] group-hover:bg-[#AAAAAA]"} duration-150`}
       >
         <span
           className={`size-5 rounded-full bg-white group-active:bg-[#EEEEEE] ${checked ? "translate-x-full" : "translate-0"} duration-150 ease-out`}
         />
-      </div>
-    </button>
+      </label>
+    </>
   );
 };
