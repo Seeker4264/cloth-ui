65b442cf27ac587cdec8ee13dadd8301bf940df9
Seeker4264 <sebashmorales@gmail.com>
2025-04-18 23:44:39 -0400

    checkbox style

--- src/components/checkbox/input.tsx
+++ src/components/checkbox/input.tsx
@@ -23,21 +23,38 @@ export const Input: React.FC<InputProps> = ({
 }) => {
   return (
     <>
-      <div>
-        <input
-          id={id}
-          required={required}
-          type="checkbox"
-          className=""
-          disabled={disabled}
-          checked={checked}
-          readOnly={readOnly}
-          onChange={onChange}
-          {...props}
-        />
-        <label htmlFor={id} className="">
-          {label}
+      <div className="flex flex-row items-center gap-2">
+        <label
+          htmlFor={id}
+          className="flex items-center cursor-pointer relative p-1"
+        >
+          <input
+            id={id}
+            required={required}
+            type="checkbox"
+            className="peer h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 checked:bg-slate-800 checked:border-slate-800"
+            disabled={disabled}
+            checked={checked}
+            readOnly={readOnly}
+            onChange={onChange}
+            {...props}
+          />
+          <span className="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none">
+            <svg
+              className="size-3.5"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              {" "}
+              <polyline points="20 6 9 17 4 12" />
+            </svg>
+          </span>
         </label>
+        <p className="text-base font-medium text-gray-900">{label}</p>
       </div>
     </>
   );
