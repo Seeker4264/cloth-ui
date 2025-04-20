2c6c802730f53c4b2b04afeb2e4d39f60948d649
Seeker4264 <sebashmorales@gmail.com>
2025-04-20 11:54:34 -0400

    carousel example styles modified

--- examples/carousel.tsx
+++ examples/carousel.tsx
@@ -11,17 +11,19 @@ const CarouselExample: React.FC<CarouselExampleProps> = ({
   autoSlideInterval,
 }) => {
   return (
-    <Carousel autoSlide={autoSlide} autoSlideInterval={autoSlideInterval}>
-      <div className="h-72 bg-[#3380EC] flex items-center justify-center text-white text-2xl font-medium">
-        Slide 1
-      </div>
-      <div className="h-72 bg-[#3AB37C] flex items-center justify-center text-white text-2xl font-medium">
-        Slide 2
-      </div>
-      <div className="h-72 bg-[#EC3333] flex items-center justify-center text-white text-2xl font-medium">
-        Slide 3
-      </div>
-    </Carousel>
+    <div className="mx-24">
+      <Carousel autoSlide={autoSlide} autoSlideInterval={autoSlideInterval}>
+        <div className="h-96 bg-[#3380EC] flex items-center justify-center text-white text-2xl font-medium">
+          Slide 1
+        </div>
+        <div className="h-96 bg-[#3AB37C] flex items-center justify-center text-white text-2xl font-medium">
+          Slide 2
+        </div>
+        <div className="h-96 bg-[#EC3333] flex items-center justify-center text-white text-2xl font-medium">
+          Slide 3
+        </div>
+      </Carousel>
+    </div>
   );
 };
 
