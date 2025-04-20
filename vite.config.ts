b78bda9c5ced867650f1eb409c233daf504e00d1
Seeker4264 <sebashmorales@gmail.com>
2025-04-20 16:12:06 -0400

    settings for build script

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.tsx"),
+      name: "cloth-ui",
+      formats: ["es", "umd"],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
 });
