86cb98933d131ae667fb2066b17e836a32d8538c
Seeker4264 <sebashmorales@gmail.com>
2025-04-20 16:15:42 -0400

    fixed issue with types on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    dts({
+      tsconfigPath: "./tsconfig.app.json",
+      insertTypesEntry: true,
+      include: ["src"],
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.tsx"),
@@ -13,11 +22,12 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "react-dom", "motion"],
       output: {
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
+          motion: "Motion",
         },
       },
     },
